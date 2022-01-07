f5f26077d90d6067e52881c9c1ffd4a45076813d
Pedroma34 <62633760+Pedroma34@users.noreply.github.com>
2022-01-07 02:38:04 -0500

    Update main.cpp

--- PeMem/main.cpp
+++ PeMem/main.cpp
@@ -4,6 +4,19 @@
 ----->Please, read comments in PeMem.h<---------
 */
 
+//Hook function example. Always end with int 3 int 3
+__declspec(naked) void ourFunction() {
+	__asm
+	{
+		push eax
+		mov eax, [esi + 0x324]
+		mov edi, eax
+		sub[esi + 0x324], di
+		pop eax
+		int 3
+		int 3
+	}
+}
 
 int main() {
 
@@ -16,19 +29,29 @@ int main() {
 		return 0; //quit program if target window is not open
 	}
 
+
+
 	/*Address. Reads from a txt file called "addresses". Must create this file and file.*/
 	pemem::Address address(&process);
 
+
+
 	/*Time. It's a trivial class. Not recommended. Use SFML clock and time classes instead.*/
 	pemem::Time time;
+
+
 
 	/*Quick test, it will READ player's health*/
 	short health = address.ReadMemory<short>(address.GetAddress("Health"));
 	std::cout << "Player's health: " << health << "\n";
 
+
+
 	/*Quick test, it will write to the player's health*/
 	short maxHealth = 2400; //value we want to write to address
 	address.WriteMemory<short>(address.GetAddress("Health"), &maxHealth);//Writing to address
+
+
 
 	/*Quick test, it will nop (write 0x90) to an instruction*/
 	address.Nop("InfiniteAmmo", address.GetAddress("AmmoDecrease"), 4); //now we have infinite ammo
@@ -36,15 +59,18 @@ int main() {
 	/*Unoping (writing original bytes to where it was nopped*/
 	address.Unop("InfiniteAmo"); //disables infinite ammo by writing original bytes to instruction
 
-	//Editing memory(bytes) in a specific way
-	address.EditBytes("EnemyHealthDecrease", address.GetAddress("EnemyHealthDecrease"), {0x83, 0xAE, 0x24, 0x03, 0x00, 0x00, 0x00 }, 7);
-	address.UneditBytes("EnemyHealthDecrease");
+
+	/*Hooking example*/
+	address.Hook("HookExample", (uintptr_t)ourFunction, address.GetAddress("EnemyHealthDecrease"), 7);
+	address.UnHook("HookExample");
 
 	/*Main loop. Alive while we have target process open.*/
 	while (process.isOpen()) {
 		time.Update();//Reseting our elapsed time every tick.
 		process.Update(time.GetElapsed());//Checking if our program is open every x seconds.
 	}
+
+
 
 	/*Program will terminate when target process is closed.*/
 	return 0;
